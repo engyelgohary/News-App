4376e98c5807b1839d04abe43ee7d048823d5be1
Your Name <you@example.com>
2024-04-25 12:27:22 +0200

    MVVM Provider News

--- lib/news/news_widget.dart
+++ lib/news/news_widget.dart
@@ -5,7 +5,9 @@ import 'package:news/api/api_manager.dart';
 import 'package:news/news/new_item.dart';
 import 'package:news/model/NewResponse.dart';
 import 'package:news/model/SourceResponse.dart';
+import 'package:news/news/news_widget_view_Model.dart';
 import 'package:news/theme/mytheme.dart';
+import 'package:provider/provider.dart';
 
 class News_details extends StatefulWidget {
   Source source;
@@ -16,61 +18,108 @@ class News_details extends StatefulWidget {
 }
 
 class _News_detialsState extends State<News_details> {
+  NewsDetailsViewModel viewModel = NewsDetailsViewModel();
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    viewModel.getNews(widget.source.id!);
+  }
   Widget build(BuildContext context) {
-    return FutureBuilder<NewResponse?>(
-        future: ApiManger.getNewsbyId(newId: widget.source.id??"",),
-         builder: (context, snapshot) {
-           if(snapshot.connectionState==ConnectionState.waiting){
+    return ChangeNotifierProvider(create: (context) => viewModel,
+      child: Consumer<NewsDetailsViewModel>(
+        builder:(context, value, child) {
+          if(viewModel.errorMessage != null){
+            return Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(viewModel.errorMessage!),
+                              ElevatedButton(onPressed: (){
+                                viewModel.getNews(widget.source.id!);
+                                setState(() {
+
+                                });
+                              }, child: const Text('Try Again'))
+                            ],
+                           );
+          }else if(viewModel.newsList == null){
             return Center(
-              child: CircularProgressIndicator(
-                backgroundColor: MyTheme.whiteColor,
-                color: MyTheme.primaryColor,
-              ),
+                          child: CircularProgressIndicator(
+                            backgroundColor: MyTheme.whiteColor,
+                            color: MyTheme.primaryColor,
+                          ),
+                        );
+          } else {
+            return ListView.separated(itemBuilder: (context, index) {
+                       return News_item(news: viewModel.newsList![index]);
+                     },
+                     itemCount: viewModel.newsList!.length,
+                     separatorBuilder: (context, index) {
+                      return Divider(
+                            color:  MyTheme.primaryColor,
+                            thickness: 2,
+                          );
+
+          }
             );
-           } else if(snapshot.hasError){
-               return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                 const Text('Smothing went Wrong'),
-                  ElevatedButton(onPressed: (){
-                    ApiManger.getNewsbyId(newId: widget.source.id??"",);
-                    setState(() {
-                      
-                    });
-                  }, child: const Text('Try Again'))
-                ],
-               );
-           }
-           if(snapshot.data?.status !='ok'){
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(snapshot.data?.message ??'Smothing went Wrong'),
-                  ElevatedButton(onPressed: (){
-                      ApiManger.getNewsbyId(newId: widget.source.id??"");
-                      setState(() {
-                        
-                      });
-                  }, child: const Text('Try Again'))
-                ],
-               );
-           }
-           var newList = snapshot.data?.articles ?? [];
-           return ListView.separated(itemBuilder: (context, index) {
-             return News_item(news: newList[index]);
-           },
-           itemCount: newList.length,
-           separatorBuilder: (context, index) {
-            return Divider(
-                  color:  MyTheme.primaryColor,
-                  thickness: 2,
-                );
-           },
-           );
-         },
+        }
+        },
+      ),
     );
+    //   FutureBuilder<NewResponse?>(
+    //     future: ApiManger.getNewsbyId(newId: widget.source.id??"",),
+    //      builder: (context, snapshot) {
+    //        if(snapshot.connectionState==ConnectionState.waiting){
+    //         return Center(
+    //           child: CircularProgressIndicator(
+    //             backgroundColor: MyTheme.whiteColor,
+    //             color: MyTheme.primaryColor,
+    //           ),
+    //         );
+    //        } else if(snapshot.hasError){
+    //            return Column(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             crossAxisAlignment: CrossAxisAlignment.center,
+    //             children: [
+    //              const Text('Smothing went Wrong'),
+    //               ElevatedButton(onPressed: (){
+    //                 ApiManger.getNewsbyId(newId: widget.source.id??"",);
+    //                 setState(() {
+    //
+    //                 });
+    //               }, child: const Text('Try Again'))
+    //             ],
+    //            );
+    //        }
+    //        if(snapshot.data?.status !='ok'){
+    //           return Column(
+    //             mainAxisAlignment: MainAxisAlignment.center,
+    //             crossAxisAlignment: CrossAxisAlignment.center,
+    //             children: [
+    //               Text(snapshot.data?.message ??'Smothing went Wrong'),
+    //               ElevatedButton(onPressed: (){
+    //                   ApiManger.getNewsbyId(newId: widget.source.id??"");
+    //                   setState(() {
+    //
+    //                   });
+    //               }, child: const Text('Try Again'))
+    //             ],
+    //            );
+    //        }
+    //        var newList = snapshot.data?.articles ?? [];
+    //        return ListView.separated(itemBuilder: (context, index) {
+    //          return News_item(news: newList[index]);
+    //        },
+    //        itemCount: newList.length,
+    //        separatorBuilder: (context, index) {
+    //         return Divider(
+    //               color:  MyTheme.primaryColor,
+    //               thickness: 2,
+    //             );
+    //        },
+    //        );
+    //      },
+    // );
   }
 }
---
+++ lib/news/news_widget_view_Model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:news/api/api_manager.dart';
+
+import '../model/NewResponse.dart';
+
+class NewsDetailsViewModel extends ChangeNotifier{
+  List<News>?newsList;
+  String?errorMessage;
+
+  void getNews(String newId) async {
+    newsList = null;
+    errorMessage = null;
+    notifyListeners();
+    try {
+      var response = await ApiManger.getNewsbyId(newId: newId);
+      if(response!.status == 'error'){
+        errorMessage = response.message;
+      } else {
+        newsList = response.articles;
+      }
+    }catch(e){
+      errorMessage = "$e";
+    }
+    notifyListeners();
+  }
+}
